9138660c562660bacd878f95cef1716dc85edc39
Oleksandr Fedosyeyev <oleksandr.fedosyeyev@passus.pl>
2019-07-11 11:25:07 +0200

    Add project files.

---
+++ NLogViewer/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace NLogViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.corelationIds = new System.Windows.Forms.ComboBox();
+            this.button_filter = new System.Windows.Forms.Button();
+            this.combo_type = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.watchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1300, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFile,
+            this.watchToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openFile
+            // 
+            this.openFile.Name = "openFile";
+            this.openFile.Size = new System.Drawing.Size(103, 22);
+            this.openFile.Text = "Open";
+            this.openFile.Click += new System.EventHandler(this.OpenFile_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 32);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1276, 406);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // corelationIds
+            // 
+            this.corelationIds.FormattingEnabled = true;
+            this.corelationIds.Location = new System.Drawing.Point(216, 1);
+            this.corelationIds.Name = "corelationIds";
+            this.corelationIds.Size = new System.Drawing.Size(209, 21);
+            this.corelationIds.TabIndex = 2;
+            // 
+            // button_filter
+            // 
+            this.button_filter.Location = new System.Drawing.Point(1037, 0);
+            this.button_filter.Name = "button_filter";
+            this.button_filter.Size = new System.Drawing.Size(75, 24);
+            this.button_filter.TabIndex = 3;
+            this.button_filter.Text = "Filter";
+            this.button_filter.UseVisualStyleBackColor = true;
+            this.button_filter.Click += new System.EventHandler(this.button_filter_Click);
+            // 
+            // combo_type
+            // 
+            this.combo_type.FormattingEnabled = true;
+            this.combo_type.Location = new System.Drawing.Point(498, 1);
+            this.combo_type.Name = "combo_type";
+            this.combo_type.Size = new System.Drawing.Size(167, 21);
+            this.combo_type.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(142, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Corelation ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(444, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Log type";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(690, 5);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Text in message";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(781, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(235, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // watchToolStripMenuItem
+            // 
+            this.watchToolStripMenuItem.Name = "watchToolStripMenuItem";
+            this.watchToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.watchToolStripMenuItem.Text = "Watch";
+            this.watchToolStripMenuItem.Click += new System.EventHandler(this.watchToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1300, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.combo_type);
+            this.Controls.Add(this.button_filter);
+            this.Controls.Add(this.corelationIds);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "NLog viewer";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFile;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox corelationIds;
+        private System.Windows.Forms.Button button_filter;
+        private System.Windows.Forms.ComboBox combo_type;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ToolStripMenuItem watchToolStripMenuItem;
+    }
+}
+
---
+++ NLogViewer/Form1.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Permissions;
+using System.Windows.Forms;
+
+namespace NLogViewer
+{
+    public partial class Form1 : Form
+    {
+        private OpenFileDialog openFileDialog1;
+        private List<string[]> fullList;
+        private List<string> guids;
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetupDataGridView();
+            FillTypeCombo();
+        }
+
+        private void FillTypeCombo()
+        {
+            combo_type.DataSource = new string[] { string.Empty, "INFO", "WARN", "DEBUG", "ERROR", "FATAL" };
+        }
+
+        private void SetupDataGridView()
+        {
+            this.dataGridView1.ColumnCount = 7;
+
+            this.dataGridView1.ColumnHeadersDefaultCellStyle.BackColor = Color.Navy;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle.Font = new Font(this.dataGridView1.Font, FontStyle.Bold);
+
+            this.dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.DisplayedCellsExceptHeaders;
+            this.dataGridView1.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.Single;
+            this.dataGridView1.CellBorderStyle = DataGridViewCellBorderStyle.Single;
+            this.dataGridView1.GridColor = Color.Black;
+            this.dataGridView1.RowHeadersVisible = false;
+
+            this.dataGridView1.Columns[0].Name = "Date";
+            this.dataGridView1.Columns[0].Width = 120;
+
+            this.dataGridView1.Columns[1].Name = "Corelation ID";
+            this.dataGridView1.Columns[1].Width = 250;
+            this.dataGridView1.Columns[1].DefaultCellStyle.Font = new Font(new FontFamily("Calibri"), 10, FontStyle.Bold);
+
+            this.dataGridView1.Columns[2].Name = "Type";
+            this.dataGridView1.Columns[2].Width = 100;
+            this.dataGridView1.Columns[2].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
+            this.dataGridView1.Columns[3].Name = "Component";
+            this.dataGridView1.Columns[3].Width = 350;
+
+            this.dataGridView1.Columns[4].Name = "Message";
+            this.dataGridView1.Columns[4].Width = 545;
+
+            this.dataGridView1.Columns[5].Name = "Url";
+            this.dataGridView1.Columns[5].Width = 300;
+
+            this.dataGridView1.Columns[6].Name = "Action";
+            this.dataGridView1.Columns[6].Width = 250;
+
+            this.dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Dock = DockStyle.Fill;
+
+            openFileDialog1 = new OpenFileDialog
+            {
+                FileName = "Select a log file",
+                Title = "Open log file"
+            };
+        }
+
+        private void OpenFile_Click(object sender, EventArgs e)
+        {
+            fullList = new List<string[]>();
+            guids = new List<string> { string.Empty };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                dataGridView1.Rows.Clear();
+
+                WriteDataFromFile();
+
+                corelationIds.DataSource = guids;
+                Format();
+            }
+        }
+
+        private void WriteDataFromFile()
+        {
+            using (Stream fileStream = File.Open(openFileDialog1.FileName, FileMode.Open))
+            using (StreamReader reader = new StreamReader(fileStream))
+            {
+                string line = null;
+                do
+                {
+                    line = reader.ReadLine();
+                    if (line == null)
+                    {
+                        break;
+                    }
+
+                    string[] parts = line.Split(new char[] { '|' }, StringSplitOptions.None);
+                    string[] row;
+                    if (parts.Length == 1)
+                    {
+                        row = new string[] { parts[0], string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty };
+                    }
+                    else if (parts.Length == 2)
+                    {
+                        row = new string[] { parts[0], parts[1], string.Empty, string.Empty, string.Empty, string.Empty, string.Empty };
+                    }
+                    else if (parts.Length == 3)
+                    {
+                        row = new string[] { parts[0], parts[1], parts[2], string.Empty, string.Empty, string.Empty, string.Empty };
+                    }
+                    else if (parts.Length == 4)
+                    {
+                        row = new string[] { parts[0], parts[1], parts[2], parts[3], string.Empty, string.Empty, string.Empty };
+                    }
+                    else if (parts.Length == 5)
+                    {
+                        row = new string[] { parts[0], parts[1], parts[2], parts[3], parts[4], string.Empty, string.Empty };
+                    }
+                    else if (parts.Length == 6)
+                    {
+                        row = new string[] { parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], string.Empty };
+                    }
+                    else if (parts.Length == 7)
+                    {
+                        row = new string[] { parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], parts[6] };
+                    }
+                    else
+                    {
+                        row = new string[] { string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty };
+                    }
+
+                    fullList.Add(row);
+                    dataGridView1.Rows.Add(row);
+                    if (parts.Length > 1 && Guid.TryParse(parts[1], out Guid newGuid))
+                    {
+                        if (!guids.Contains(newGuid.ToString()))
+                            guids.Add(newGuid.ToString());
+                    }
+
+                } while (true);
+            }
+        }
+
+        private void Format()
+        {
+            foreach (DataGridViewRow Myrow in dataGridView1.Rows)
+            {
+                if (Myrow.Cells[2].Value?.ToString() == "INFO")
+                {
+                    Myrow.Cells[2].Style.BackColor = Color.LightBlue;
+                }
+                else if (Myrow.Cells[2].Value?.ToString() == "WARN")
+                {
+                    Myrow.Cells[2].Style.BackColor = Color.LightPink;
+                }
+                else if (Myrow.Cells[2].Value?.ToString() == "DEBUG")
+                {
+                    Myrow.Cells[2].Style.BackColor = Color.LightYellow;
+                }
+                else if (Myrow.Cells[2].Value?.ToString() == "ERROR")
+                {
+                    Myrow.Cells[2].Style.BackColor = Color.Tomato;
+                }
+                else if (Myrow.Cells[2].Value?.ToString() == "FATAL")
+                {
+                    Myrow.Cells[2].Style.BackColor = Color.Red;
+                }
+                else
+                {
+                    Myrow.Cells[2].Style.BackColor = Color.LightGray;
+                }
+            }
+        }
+
+        private void button_filter_Click(object sender, EventArgs e)
+        {
+            var filteredList = fullList;
+            dataGridView1.Rows.Clear();
+            var hasFilter = false;
+
+            if (!string.IsNullOrEmpty(corelationIds.SelectedValue?.ToString()))
+            {
+                hasFilter = true;
+                filteredList = filteredList.Where(x => x[1] == corelationIds.SelectedValue.ToString()).ToList();
+            }
+
+            if (combo_type.SelectedValue.ToString() != string.Empty)
+            {
+                hasFilter = true;
+                filteredList = filteredList.Where(x => x[2] == combo_type.SelectedValue.ToString()).ToList();
+            }
+
+            if (textBox1.Text != string.Empty)
+            {
+                hasFilter = true;
+                filteredList = filteredList.Where(x => x[4].Contains(textBox1.Text)).ToList();
+            }
+
+            if (!hasFilter)
+            {
+                filteredList = fullList;
+            }
+
+            filteredList?.ForEach(x =>
+            {
+                dataGridView1.Rows.Add(x);
+            });
+
+            Format();
+        }
+
+        [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+        private void watchToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFile_Click(null, null);
+
+            var watch = new FileSystemWatcher();
+            watch.Path = openFileDialog1.FileName.Replace($"\\{openFileDialog1.SafeFileName}", "");
+            watch.Filter = openFileDialog1.SafeFileName;
+            watch.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite;
+            watch.Changed += new FileSystemEventHandler(OnChanged);
+            watch.EnableRaisingEvents = true;
+        }
+
+        private void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            DateTime.TryParse(dataGridView1.Rows[dataGridView1.Rows.GetLastRow(DataGridViewElementStates.None) - 1].Cells[0].Value.ToString(), out DateTime lastRowDate);
+
+            using (Stream fileStream = File.Open(openFileDialog1.FileName, FileMode.Open))
+            using (StreamReader reader = new StreamReader(fileStream))
+            {
+                string line = null;
+                do
+                {
+                    line = reader.ReadLine();
+                    if (line == null)
+                    {
+                        break;
+                    }
+
+                    string[] parts = line.Split(new char[] { '|' }, StringSplitOptions.None);
+
+                    DateTime.TryParse(parts[0], out DateTime rowDate);
+
+                    if (rowDate > lastRowDate)
+                    {
+                        string[] row;
+                        if (parts.Length == 1)
+                        {
+                            row = new string[] { parts[0], string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty };
+                        }
+                        else if (parts.Length == 2)
+                        {
+                            row = new string[] { parts[0], parts[1], string.Empty, string.Empty, string.Empty, string.Empty, string.Empty };
+                        }
+                        else if (parts.Length == 3)
+                        {
+                            row = new string[] { parts[0], parts[1], parts[2], string.Empty, string.Empty, string.Empty, string.Empty };
+                        }
+                        else if (parts.Length == 4)
+                        {
+                            row = new string[] { parts[0], parts[1], parts[2], parts[3], string.Empty, string.Empty, string.Empty };
+                        }
+                        else if (parts.Length == 5)
+                        {
+                            row = new string[] { parts[0], parts[1], parts[2], parts[3], parts[4], string.Empty, string.Empty };
+                        }
+                        else if (parts.Length == 6)
+                        {
+                            row = new string[] { parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], string.Empty };
+                        }
+                        else if (parts.Length == 7)
+                        {
+                            row = new string[] { parts[0], parts[1], parts[2], parts[3], parts[4], parts[5], parts[6] };
+                        }
+                        else
+                        {
+                            row = new string[] { string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty };
+                        }
+
+                        fullList.Add(row);
+                        dataGridView1.Invoke(new Action(() => dataGridView1.Rows.Add(row)));
+                        if (parts.Length > 1 && Guid.TryParse(parts[1], out Guid newGuid))
+                        {
+                            if (!guids.Contains(newGuid.ToString()))
+                                guids.Add(newGuid.ToString());
+                        }
+                    }
+
+                } while (true);
+            }
+
+            Format();
+            corelationIds.DataSource = guids;
+        }
+    }
+}
